93f777a347ba10d1e521a51a35b82366b80cb0ea
disitec <fjmartinez@disitec.es>
2016-12-22 18:27:21 +0100

    add command for testing

---
+++ src/Commands/ScaffoldCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\User;
+use App\DripEmailer;
+use Illuminate\Console\Command;
+
+class Scaffold extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'disitec:scaffold {model}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Scaffold Model, Controller and Views';
+
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $model = $this->argument('model');
+        $this->info("Generando scaffolding para $model");
+    }
+}
